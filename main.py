c2f83b9c98da5f662c4de0803c67bb66aca3b648
gegendepressed <sairaj09pai@gmail.com>
2024-03-15 21:36:03 +0530

    added beautification and datetime fix

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import os
 import datetime
+from pytermgui import tim
+import signal
+import sys
+signal.signal(signal.SIGINT, lambda x, y: sys.exit(0))
 
+tim.print("[bold !gradient(56)]Diary[/!]")
 
-
-print("\n >>>>>>>>>>>>> Menu <<<<<<<<<<<<<<<")
-print(">> 1. Decrypt and View Entries <<")
-print(">> 2. Encrypts and add Entries <<")
-print(">> 3. Add in existing file <<")
-print(">> 3. Remove a Entry <<")
+tim.print("[bold]1. Create New and Encrypt [/]")
+tim.print("[bold]2. Decrypt and View [/]")
+tim.print("[bold]3. Add to existing[/]")
+tim.print("[bold red]4. Remove Entry[/]")
+tim.print("Support me [skyblue underline ~https://github.com/gegendepressed]" + "on Github[/]!")
 
 choice = input("Enter a choice : ")
 
+
 if choice == "1":
-        print("Enter the file you want to view: ")
-        filename = input("")
-        if os.path.exists(filename): 
-                os.system(f"gpg -d {filename}")
-elif choice == "2":
         print("To proceed you would have to create a file to keep the entries stored : ")
         filename = input("")
 
         with open(filename,"a") as file: 
                 current_date= datetime.datetime.now()
-                date = str(current_date)
+                date = str(current_date.strftime("Date : %D-%M-%Y Time : %H:%M:%S"))
                 task = input("Diary Entry : ")
                 file.write(date + "\n")
                 file.write(task + "\n")
@@ -30,9 +30,14 @@ elif choice == "2":
         if os.path.exists(filename):
                 os.system(f"gpg --symmetric {filename}")
         os.remove(filename)
+elif choice == "2":
+        print("Enter the file you want to view: ")
+        filename = input("")
+        if os.path.exists(filename): 
+                os.system(f"gpg -d {filename}")
 
 elif choice == "3":
-        filename = input("")
+        filename = input("Enter the file you want to add : ")
         if os.path.exists(filename):
                 os.system(f"gpg -d {filename}")
         with open(filename,"a") as file: 
@@ -44,8 +49,8 @@ elif choice == "3":
                 print("Entry added successfully")
 
 elif choice == "4":
-        print("Do you want to remove a file (note that the entry will be forever lost to the sands of time) : ")
-        print("(y/n)")
+        tim.print("Do you want to [bold red]remove[/] a file (note that the entry will be forever lost to the sands of time) : ")
+        tim.print("[bold lightblue](Y/N)[/fg italic]!")
         if "y" or "Y" or "Yes":
                 filename = input("Enter the filename : ")
                 os.remove(filename) 
@@ -53,3 +58,4 @@ elif choice == "4":
                print("Nevermind.")     
 else :
      print("Well, thank you for testing this humble program.")
+
